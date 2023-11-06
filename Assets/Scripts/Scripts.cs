a5c593c909170aea958a8b4a7f193c16be068a8c
feerraan <fazpiazuadrover@gmail.com>
2023-11-06 20:35:06 +0100

    Acabat

--- Assets/Scripts/Scripts.cs
+++ Assets/Scripts/Scripts.cs
@@ -21,34 +21,36 @@ public class Scripts : MonoBehaviour
     [SerializeField] private int months = 0;
     [SerializeField] private int years = 0;
 
-    //Variables Exercici ??
-
-    [SerializeField] private int num3 = 4;
-    [SerializeField] private int num4 = 8;
 
     //Variables Exercici 4
 
-    [SerializeField] private string nota;
+    [SerializeField] private int year;
+
+    //Variables Exercici 8
+    [SerializeField] private float basetriangulo;
+    [SerializeField] private float alturatriangulo;
+
+    //Variable Exercici 10
+   
+        [SerializeField] private float impar;
+    
+    
 
 
-    // Variables Exercici 8
 
-    [SerializeField] private string diasetmana;
-
-    //Variables Exercici 9
-
-    [SerializeField] private int edat;
-    [SerializeField] private string rango;
-
-    // Variables Exercici 10
-
-    [SerializeField] private float angle1;
-    [SerializeField] private float angle2;
-    [SerializeField] private float angle3;
-
-
-    private void Start()
+private void Start()
     {
+
+        //Exercici 10
+        if (impar % 2 == 0)
+        {
+            Debug.Log($"true");
+        }
+        else
+        {
+            Debug.Log($"false");
+        }
+
 
         //Exercici 1
 
@@ -114,258 +116,37 @@ public class Scripts : MonoBehaviour
 
         //Exercici 4
 
-        if (nota == "A")
-        {
-            Debug.Log("excellent");
-        }
-        else if (nota == "B")
-        {
-            Debug.Log("notable");
-        }
-        else if (nota == "C")
-        {
-            Debug.Log("passed");
-        }
-        else if (nota == "D")
-        {
-            Debug.Log("fail");
-        }
-
-        //ejercicio 5
-
-
-        if (months == 1 || months == 3 || months == 5 || months == 7 || months == 9 || months == 11)
-        {
-            if (days <= 31 && days > 0 && years > 0)
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             {
-                Debug.Log($" {days} / {months} / {years} es una fetxa valida");
+            Debug.Log($"true");
             }
             else
             {
-                Debug.Log(" aixo no es una fetxa valid");
-            }
-
+            Debug.Log($"false");
         }
-        else if (months == 2)
-        {
-            if (days <= 28 && days > 0 && years > 0)
-            {
-                Debug.Log($" {days} / {months} / {years} es una fetxa valida");
-            }
-            else
-            {
-                Debug.Log(" aixo no es una fetxa valid");
-            }
+       
 
-        }
-        else if (months == 4 || months == 6 || months == 8 || months == 10 || months == 12)
-        {
-            if (days <= 30 && days > 0 && years > 0)
-            {
-                Debug.Log($" {days} / {months} / {years} es una fetxa valida");
-
-            }
-            else
-            {
-                Debug.Log(" aixo no es una fetxa valida");
-            }
-        }
-        else
-        {
-            Debug.Log("aixo no es una fetxa valida");
-        }
-
-
-        //Exercici 7
-
-        if (months == 1 && days >= 1 && days < 20)
-        {
-
-            Debug.Log("el teu horoscopo es capricornio");
-        }
-        else if (months == 1 && days >= 20 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es acuario");
-        }
-        else if (months == 2 && days >= 1 && days < 19)
-        {
-
-            Debug.Log("el teu horoscopo es acuario");
-        }
-        else if (months == 2 && days >= 19 && days < 29)
-        {
-
-            Debug.Log("el teu horoscopo es piscis");
-        }
-        else if (months == 3 && days >= 1 && days < 21)
-        {
-
-            Debug.Log("el teu horoscopo es piscis");
-        }
-        else if (months == 3 && days >= 21 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es aries");
-        }
-        else if (months == 4 && days >= 1 && days < 20)
-        {
-
-            Debug.Log("el teu horoscopo es aries");
-        }
-        else if (months == 4 && days >= 21 && days < 31)
-        {
-
-            Debug.Log("el teu horoscopo es tauro");
-        }
-        else if (months == 5 && days >= 1 && days < 21)
-        {
-
-            Debug.Log("el teu horoscopo es tauro");
-        }
-        else if (months == 5 && days >= 21 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es geminis");
-        }
-        else if (months == 6 && days >= 1 && days < 21)
-        {
-
-            Debug.Log("el teu horoscopo es geminis");
-        }
-        else if (months == 6 && days >= 21 && days < 31)
-        {
-
-            Debug.Log("el teu horoscopo es cancer");
-        }
-        else if (months == 7 && days >= 1 && days < 23)
-        {
-
-            Debug.Log("el teu horoscopo es cancer");
-        }
-        else if (months == 7 && days >= 23 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es leo");
-        }
-        else if (months == 8 && days >= 1 && days < 23)
-        {
-
-            Debug.Log("el teu horoscopo es leo");
-        }
-        else if (months == 8 && days >= 23 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es virgo");
-        }
-        else if (months == 9 && days >= 1 && days < 23)
-        {
-
-            Debug.Log("el teu horoscopo es virgo");
-        }
-        else if (months == 9 && days >= 23 && days < 31)
-        {
-
-            Debug.Log("el teu horoscopo es libra");
-        }
-        else if (months == 10 && days >= 1 && days < 23)
-        {
-
-            Debug.Log("el teu horoscopo es libra");
-        }
-        else if (months == 10 && days >= 23 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es escorpio");
-        }
-        else if (months == 11 && days >= 1 && days < 22)
-        {
-
-            Debug.Log("el teu horoscopo es escorpio");
-        }
-        else if (months == 11 && days >= 22 && days < 31)
-        {
-
-            Debug.Log("el teu horoscopo es sagitario");
-        }
-        else if (months == 12 && days >= 1 && days < 22)
-        {
-
-            Debug.Log("el teu horoscopo es sagitario");
-        }
-        else if (months == 12 && days >= 22 && days < 32)
-        {
-
-            Debug.Log("el teu horoscopo es capricornio");
-        }
 
         //Exercici 8
 
-        if (diasetmana == "dilluns" || diasetmana == "dimarts" || diasetmana == "dimecres" || diasetmana == "dijous" || diasetmana == "divendres")
+        if (basetriangulo <= 0 || alturatriangulo <= 0)
         {
-            Debug.Log("no es cap de setmana");
-        }
-        else
-        {
-            Debug.Log("es cap de setmana");
+            throw new ArgumentException("La base y la altura tienen que ser positivos");
         }
 
-        //Exercici 9
+        Debug.Log($"La base del triangulo es {0.5 * basetriangulo * alturatriangulo}");
 
-        if (edat >= 18 && edat < 65)
-        {
-            if (rango == "or")
-            {
-                Debug.Log("tens un 75% de descompte");
-            }
-            else if (rango == "plata")
-            {
-                Debug.Log("tens un 50% de descompte");
-            }
-            else if (rango == "bronze")
-            {
-                Debug.Log("tens un 25% de descompte");
-            }
+        //Exercici 
 
-        }
-        else if (edat > 65)
-        {
-            if (rango == "or")
-            {
-                Debug.Log("tens un 50% de descompte");
-            }
-            else if (rango == "plata")
-            {
-                Debug.Log("tens un 25% de descompte");
-            }
-            else if (rango == "bronze")
-            {
-                Debug.Log("tens un 15% de descompte");
-            }
-        }
-        else if (edat < 18)
-        {
-            Debug.Log("no pots comprar a la tenda perque ets menor");
-        }
-
-        //Exercici 10
-
-
-        if (angle1 + angle2 + angle3 == 180)
-        {
-            Debug.Log("aquests angles poden ser els interns d'un triangle");
-        }
-        if (angle1 + angle2 + angle3 != 180)
-        {
-            Debug.Log("aquests angles no poden ser els interns d'un triangle");
-        }
+        DateTime date = new DateTime(years, months, days);
+        date = date.AddDays(- 1);
+        Debug.Log(date);
 
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
     }
-}
+
+    
+    
